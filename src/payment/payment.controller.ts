660cd87b74c28c38da9f3ab98f16977242a7a5d3
p2foundation <p2found@gmail.com>
2023-09-27 07:58:20 +0000

    completed school fees api

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
+import { PaymentService } from "./payment.service";
+import { Payment } from "./payment.entity";
+import { Account } from "../account/account.entity";
 
-@Controller('payment')
-export class PaymentController {}
+@Controller("payments")
+export class PaymentController {
+  private logger = new Logger(PaymentService.name);
+
+  constructor(private readonly paymentService: PaymentService) {
+  }
+
+  @Get()
+  async findAllPayment(): Promise<Payment[]> {
+    const allPay = await this.paymentService.getAllPayments();
+    this.logger.debug(`find all payment ===> ${JSON.stringify(allPay)}`)
+    return allPay
+  }
+
+  @Post()
+  async create(@Body() payment: Payment): Promise<Payment> {
+    const pays = await this.paymentService.create(payment);
+    this.logger.debug(`create payment ===> ${JSON.stringify(pays)}`)
+    return pays;
+  }
+}
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Payment } from "./payment.entity";
+import { Account } from "../account/account.entity";
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>
+  ) {
+  }
+
+  async create(payment: Payment): Promise<Payment> {
+    return this.paymentRepository.save(payment);
+  }
+
+
+
+  async getAllPayments(): Promise<Payment[]> {
+    return this.paymentRepository.find();
+  }
+}
